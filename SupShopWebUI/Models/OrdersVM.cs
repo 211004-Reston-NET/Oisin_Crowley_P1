92e4cf9fe228ad4cae868d01dbf235eb042bdf3b
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-11-11 17:52:14 -0500

    Orders changes

--- SupShopWebUI/Models/OrdersVM.cs
+++ SupShopWebUI/Models/OrdersVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupplyShopModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,18 @@ namespace SupShopWebUI.Models
 {
     public class OrdersVM
     {
+
+        public OrdersVM()
+        {
+
+        }
+        public OrdersVM(Orders p_orders)
+        {
+            this.OrderID = p_orders.OrderID;
+            this.StoreID = p_orders.StoreId;
+            this.TotalPrice = p_orders.totalPrice;
+            this.CustomerID = p_orders.CustomerID;
+        }
         public int OrderID { get; set; }
 
         public double TotalPrice { get; set; }
