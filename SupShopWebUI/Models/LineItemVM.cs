57d2439fee360715e0f48e2dd6632f875b8e84dc
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-11-12 15:16:39 -0500

    Lineitems

---
+++ SupShopWebUI/Models/LineItemVM.cs
@@ -0,0 +1,31 @@
+﻿using SupplyShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupShopWebUI.Models
+{
+    public class LineItemVM
+    {
+
+        public LineItemVM()
+        {
+
+        }
+        public LineItemVM(LineItems p_lineitems)
+        {
+            this.LineItemID = p_lineitems.LineItemID;
+            this.OrdersID = p_lineitems.OrdersID;
+            this.ProductID = p_lineitems.ProductID;
+            this.Quantity = p_lineitems.Quantity;
+        }
+        public int LineItemID { get; set; }
+
+        public int OrdersID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ProductID { get; set; }
+    }
+}
